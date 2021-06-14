3375a9af7b3ad91fb082442b81c731e205bb5f65
chwnk <chowaniak.bartosz@protonmail.com>
2021-06-14 16:27:01 +0200

    Created separate image componend
    Created separate image componend

--- src/components/TopBar/Logo/Logo.tsx
+++ src/components/TopBar/Logo/Logo.tsx
@@ -1,18 +1,16 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components';
+
+// Components
+import Image from 'components/common/Image/Image'
 
 // Assets
 import logo from 'assets/logo.png';
 
-const Image = styled.img`
-    max-width: 36px;
-`;
-
 const Logo: FC = () => {
     return (
         <Link to="/">
-            <Image src={logo}  alt="Company's logo" title="MyCompanyName" />
+            <Image src={logo}  alt="Company's logo" title="MyCompanyName" height={36} />
         </Link>
     )
 }
